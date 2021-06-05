f2fb0e26be04952247b283438c423568cb042c43
rodlferreira <rodriggo.lferreira@icloud.com>
2021-06-05 20:57:37 -0300

    section team finished less body piercing

--- src/components/Team/index.tsx
+++ src/components/Team/index.tsx
@@ -1,11 +1,11 @@
-import { Image, Grid, Flex, Box, Text, Button, Stack, ButtonGroup} from "@chakra-ui/react"
+import { Image, Grid, Flex, Box, Text, Button, Stack, ButtonGroup, GridItem} from "@chakra-ui/react"
 import React from "react";
 
 export default function Team() {
     return(
-        <Grid mt="3rem">
+        <Grid mt="7rem" ml="6rem">
 
-            <Stack position="absolute" ml="40rem" justify="center" marginTop="-0.75rem">
+            <Stack position="absolute" ml="32rem" justify="center" marginTop="-4.5rem">
                 <Text fontSize="45">
                     Team
                 </Text>
@@ -109,6 +109,318 @@ export default function Team() {
 
                 </Flex>
                 
+            </Grid>
+
+
+
+
+
+
+
+
+
+
+
+            <Grid position="absolute" ml="28rem">
+                <Flex>
+                <Image
+                    boxSize="13.5rem"
+                    src="/images/yrlan.jpg"
+                    alt="Yrlan Lins" 
+                    margin="0.5rem"
+
+                />
+                </Flex>
+                <Flex>
+                   <Box
+                    mt="0.5rem" 
+                    justify="center" 
+                    align="center" 
+                    ml="4.5rem"
+                   >
+                    <Text 
+                      size="xs" 
+                      textAlign="center"
+                    >
+                      Yrlan Lins
+                    </Text>
+
+                    </Box>
+
+                    <Stack 
+                      mt="2.5rem" 
+                      justify="center" 
+                      ml="2rem" 
+                      position="absolute"
+                    >
+                      <Button
+                       backgroundColor="var(--body)" 
+                       color="var(--white)" 
+                       variant="link" 
+                      >
+                        American traditional
+                      </Button>
+
+                    </Stack>
+
+                </Flex>
+                
+            </Grid>
+
+
+
+
+
+
+
+            <Grid position="absolute" ml="42rem">
+                <Flex>
+                <Image
+                    boxSize="13.5rem"
+                    src="/images/yrlan.jpg"
+                    alt="Yrlan Lins" 
+                    margin="0.5rem"
+
+                />
+                </Flex>
+                <Flex>
+                   <Box
+                    mt="0.5rem" 
+                    justify="center" 
+                    align="center" 
+                    ml="4.5rem"
+                   >
+                    <Text 
+                      size="xs" 
+                      textAlign="center"
+                    >
+                      Yrlan Lins
+                    </Text>
+
+                    </Box>
+
+                    <Stack 
+                      mt="2.5rem" 
+                      justify="center" 
+                      ml="2rem" 
+                      position="absolute"
+                    >
+                      <Button
+                       backgroundColor="var(--body)" 
+                       color="var(--white)" 
+                       variant="link" 
+                      >
+                        American traditional
+                      </Button>
+
+                    </Stack>
+
+                </Flex>
+                
+            </Grid>
+
+
+
+
+
+
+
+
+
+
+            <Grid position="absolute" ml="56rem">
+                <Flex>
+                <Image
+                    boxSize="13.5rem"
+                    src="/images/yrlan.jpg"
+                    alt="Yrlan Lins" 
+                    margin="0.5rem"
+
+                />
+                </Flex>
+                <Flex>
+                   <Box
+                    mt="0.5rem" 
+                    justify="center" 
+                    align="center" 
+                    ml="4.5rem"
+                   >
+                    <Text 
+                      size="xs" 
+                      textAlign="center"
+                    >
+                      Yrlan Lins
+                    </Text>
+
+                    </Box>
+
+                    <Stack 
+                      mt="2.5rem" 
+                      justify="center" 
+                      ml="2rem" 
+                      position="absolute"
+                    >
+                      <Button
+                       backgroundColor="var(--body)" 
+                       color="var(--white)" 
+                       variant="link" 
+                      >
+                        American traditional
+                      </Button>
+
+                    </Stack>
+
+                </Flex>
+                
+            </Grid>
+
+            <Grid mt="10rem">
+            <Grid position="absolute" ml="14rem">
+                <Flex>
+                <Image
+                    boxSize="13.5rem"
+                    src="/images/yrlan.jpg"
+                    alt="Yrlan Lins" 
+                    margin="0.5rem"
+
+                />
+                </Flex>
+                <Flex>
+                   <Box
+                    mt="0.5rem" 
+                    justify="center" 
+                    align="center" 
+                    ml="4.5rem"
+                   >
+                    <Text 
+                      size="xs" 
+                      textAlign="center"
+                    >
+                      Yrlan Lins
+                    </Text>
+
+                    </Box>
+
+                    <Stack 
+                      mt="2.5rem" 
+                      justify="center" 
+                      ml="2rem" 
+                      position="absolute"
+                    >
+                      <Button
+                       backgroundColor="var(--body)" 
+                       color="var(--white)" 
+                       variant="link" 
+                      >
+                        American traditional
+                      </Button>
+
+                    </Stack>
+
+                </Flex>
+                
+            </Grid>
+
+
+
+
+
+
+            <Grid position="absolute" ml="28rem">
+                <Flex>
+                <Image
+                    boxSize="13.5rem"
+                    src="/images/yrlan.jpg"
+                    alt="Yrlan Lins" 
+                    margin="0.5rem"
+
+                />
+                </Flex>
+                <Flex>
+                   <Box
+                    mt="0.5rem" 
+                    justify="center" 
+                    align="center" 
+                    ml="4.5rem"
+                   >
+                    <Text 
+                      size="xs" 
+                      textAlign="center"
+                    >
+                      Yrlan Lins
+                    </Text>
+
+                    </Box>
+
+                    <Stack 
+                      mt="2.5rem" 
+                      justify="center" 
+                      ml="2rem" 
+                      position="absolute"
+                    >
+                      <Button
+                       backgroundColor="var(--body)" 
+                       color="var(--white)" 
+                       variant="link" 
+                      >
+                        American traditional
+                      </Button>
+
+                    </Stack>
+
+                </Flex>
+                
+            </Grid>
+
+
+
+
+            <Grid position="absolute" ml="42rem">
+                <Flex>
+                <Image
+                    boxSize="13.5rem"
+                    src="/images/yrlan.jpg"
+                    alt="Yrlan Lins" 
+                    margin="0.5rem"
+
+                />
+                </Flex>
+                <Flex>
+                   <Box
+                    mt="0.5rem" 
+                    justify="center" 
+                    align="center" 
+                    ml="4.5rem"
+                   >
+                    <Text 
+                      size="xs" 
+                      textAlign="center"
+                    >
+                      Yrlan Lins
+                    </Text>
+
+                    </Box>
+
+                    <Stack 
+                      mt="2.5rem" 
+                      justify="center" 
+                      ml="2rem" 
+                      position="absolute"
+                    >
+                      <Button
+                       backgroundColor="var(--body)" 
+                       color="var(--white)" 
+                       variant="link" 
+                      >
+                        American traditional
+                      </Button>
+
+                    </Stack>
+
+                </Flex>
+                
+            </Grid>
+
             </Grid>
 
         </Grid>
